f6b838073341f47ccf97bad8da2de6b17f686dc2
Jefferson Brandao <jeffersonbraster@gmail.com>
2019-10-17 21:24:45 -0300

    Colocando o editar

--- src/main/java/manegedBean/UsuarioPessoaManagedBean.java
+++ src/main/java/manegedBean/UsuarioPessoaManagedBean.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.view.ViewScoped;
+import javax.faces.bean.ViewScoped;
 
 import dao.DaoGeneric;
 import model.UsuarioPessoa;
